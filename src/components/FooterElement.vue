97104c5e440b85b69d104b1ff96fc329aa7fb245
COMBOM233 <1979664967@qq.com>
2025-06-28 15:46:21 +0800

    none

--- src/components/FooterElement.vue
+++ src/components/FooterElement.vue
@@ -3,7 +3,7 @@
     <div class="bg-stone-700 w-full flex justify-center items-center md:justify-evenly flex-col md:flex-row gap-8 p-8">
         <!-- Overview start -->
         <div class="text-stone-100 text-center">
-            <p class="tracking-widest font-semibold text-stone-100">LANDING</p>
+            <p class="tracking-widest font-semibold text-stone-100">捕风捉影</p>
             <p class="tracking-widest font-light text-stone-100">PHOTOGRAPHY</p>
         </div>
         <!-- Overview end -->
